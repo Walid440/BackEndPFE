c4ec03f8f1198774164c711ff386d4dd435d5e82
walid <walid.gammoudi@esprit.tn>
2023-09-19 23:46:06 +0200

    premier commit

---
+++ src/app/main/apps/Offre/offre.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListOffreComponent } from './list-offre/list-offre.component';
+import { UpdateOffreComponent } from './update-offre/update-offre.component';
+import { AddOffreComponent } from './add-offre/add-offre.component';
+
+const routes: Routes = [
+  {
+    path: 'listOff',
+    component: ListOffreComponent,
+  }
+];
+@NgModule({
+  declarations: [
+  
+  ],
+  imports: [
+    CommonModule, RouterModule.forChild(routes)
+  ]
+})
+export class OffreModule { }
